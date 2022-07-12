6dd9fac64ed62f8f8b21314e246bcf00aa7f2e50
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-07-13 07:43:23 +0800

    plantito cart icon from 30rem to 30px

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
         }),
 
         success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/canceled`,
+        cancel_url: `${req.headers.origin}/`,
       };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
