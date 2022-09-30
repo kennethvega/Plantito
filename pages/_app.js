2d979b858c7dde4c221455363b8efdaa2f1304dc
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-09-30 14:46:33 +0800

    bug transition quck fix

--- pages/_app.js
+++ pages/_app.js
@@ -14,8 +14,7 @@ function MyApp({ Component, pageProps, router }) {
             key={router.route}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.3, ease: "easeOut" }}
-            exit={{ opacity: 0, duration: 0.5 }}
+            exit={{ opacity: 0 }}
           >
             <Component {...pageProps} />
           </motion.div>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,19 @@ import React from "react";
 import { client } from "../lib/client";
 import { Hero, FeaturedBrands } from "../components";
 import Plants from "../components/Plants";
+import { motion } from "framer-motion";
+
 const Home = ({ products, bestsellers }) => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <Hero />
       <FeaturedBrands />
       <Plants products={products} bestsellers={bestsellers} />
-    </>
+    </motion.div>
   );
 };
 
